d54456fdb9b2ccbc140c0db6e2820d8160c61c84
skote05 <144415783+skote05@users.noreply.github.com>
2025-03-18 16:27:28 +0530

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>  // Include the input-output stream header
+
+using namespace std;  // Use the standard namespace
+
+int main() {
+    cout << "Hello, World!" << endl;  // Output Hello, World! to the console
+    return 0;  // Return 0 to indicate successful execution
+}
